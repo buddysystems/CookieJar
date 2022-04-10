bec65c6222c43f51fcb0f19c6c0e7a82ff7a7a3e
Rylen Lanning <rlanning@greylionsoftware.com>
2022-04-10 16:47:57 -0500

    Addded extra columns in populateCookieTable method

--- js/popup.js
+++ js/popup.js
@@ -4,14 +4,34 @@ const getCookiesBtn = document.getElementById("getCookies")
 
 
 async function populateCookieTable() {
-    const cookies = await getCookies();
-    // for (const cookie of cookies) {
-    const cookieTableRowItem = document.createElement("tr");
+    // const cookies = await getCookies();
+    testList = TestCookie.getTestCookieList
+    for (const cookie of testList) {
+    var cookieTableRowItem = document.createElement("tr");
     cookieTableRowItem.style.marginBottom = "10px";
     cookieTableRowItem.innerHTML = `<td>${cookie.domain}</td> <td>2</td>`;
     cookieTable.appendChild(cookieTableRowItem);
-    // }
+    }
 
 }
 
 getCookiesBtn.addEventListener("click", populateCookieTable);
+
+
+class TestCookie {
+    constructor(domain, name, storeId, isStored=false) {
+        this.domain = chromeCookie.domain;
+        this.name = chromeCookie.name;
+        this.storeId = chromeCookie.storeId;
+        this.isStored = isStored
+    }
+
+    getTestCookieList() {
+        var testCookieList
+        for(i = 0; i < 10; i++) {
+            newTestCookie = TestCookie("Domain", "Name", "storeID", false)
+            testCookieList.appendChild(newTestCookie)
+        }
+        return testCookieList
+    }
+}
