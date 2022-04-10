3d1c04ad663eaf0576b605448770185a9c7d7d17
Rylen Lanning <rlanning@greylionsoftware.com>
2022-04-10 16:54:29 -0500

    Added extra columns to populateCookieTable method

--- js/popup.js
+++ js/popup.js
@@ -2,48 +2,14 @@ const cookieTable = document.getElementById("cookieTable");
 const getCookiesBtn = document.getElementById("getCookies");
 
 async function populateCookieTable() {
-<<<<<<< HEAD
-    // const cookies = await getCookies();
-    testList = TestCookie.getTestCookieList
-    for (const cookie of testList) {
-    var cookieTableRowItem = document.createElement("tr");
-=======
     const cookies = await getCookies();
-    console.log(cookies);
     // for (const cookie of cookies) {
     const cookieTableRowItem = document.createElement("tr");
->>>>>>> c81832c5a2ce502cf4da21f52df88458d7543df4
     cookieTableRowItem.style.marginBottom = "10px";
-    cookieTableRowItem.innerHTML = `<td>cookie domain test</td> <td>2</td>`;
+    cookieTableRowItem.innerHTML = `<td>${cookie.domain}</td> <td>${cookie.name}</td> <td>${cookie.path}</td>
+    <td>${cookie.secure}</td> <td>${cookie.session}</td> <td>${cookie.storeId}</td> <td>${cookie.url}</td>`;
     cookieTable.appendChild(cookieTableRowItem);
-<<<<<<< HEAD
-    }
-
-}
-
-getCookiesBtn.addEventListener("click", populateCookieTable);
-
-
-class TestCookie {
-    constructor(domain, name, storeId, isStored=false) {
-        this.domain = chromeCookie.domain;
-        this.name = chromeCookie.name;
-        this.storeId = chromeCookie.storeId;
-        this.isStored = isStored
-    }
-
-    getTestCookieList() {
-        var testCookieList
-        for(i = 0; i < 10; i++) {
-            newTestCookie = TestCookie("Domain", "Name", "storeID", false)
-            testCookieList.appendChild(newTestCookie)
-        }
-        return testCookieList
-    }
-}
-=======
     // }
 }
 
 getCookiesBtn.addEventListener("click", populateCookieTable);
->>>>>>> c81832c5a2ce502cf4da21f52df88458d7543df4
