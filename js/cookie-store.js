0a4853479bd7ac0f67f674757d69685f9f480bed
Andrew Richerson <ajorigami1@gmail.com>
2022-04-10 21:44:20 -0500

    Add cookie jar class for managing the stored cookies, and fix bug where domains preceded with a period couldn't be set in the cookie api

--- js/cookie-store.js
+++ js/cookie-store.js
@@ -9,9 +9,73 @@ function getCookieUrl(cookie) {
     );
 }
 
-class Cookie {
+const COOKIE_JAR = "COOKIE_JAR";
+class CookieJar {
+    async init() {
+        const alreadyStored = await chrome.storage.local.get(COOKIE_JAR);
+
+        if (!alreadyStored.length) {
+            await chrome.storage.local.set({ COOKIE_JAR: [] });
+        }
+    }
+
+    async addCookie(cookie) {
+        const inJar = await this.getJarCookies();
+        console.dir(inJar);
+        inJar.push(cookie);
+        await chrome.storage.local.set({ COOKIE_JAR: inJar });
+
+        const x = await this.getJarCookies();
+        console.dir(x);
+    }
+
+    async getCookie(cookieDetails) {
+        const inJar = await this.getJarCookies();
+        return inJar.find(
+            (c) =>
+                c.name == cookieDetails.name &&
+                c.storeId == cookieDetails.storeId &&
+                c.url == cookieDetails.url
+        );
+    }
+
+    async removeCookie(cookieDetails) {
+        console.log("removing 1 cookie");
+        let inJar = await this.getJarCookies();
+        // Remove the cookie matching the cookie details
+        inJar = inJar.filter(
+            (c) =>
+                c.name != cookieDetails.name &&
+                c.storeId != cookieDetails.storeId &&
+                c.url != cookieDetails.url
+        );
+        await chrome.storage.local.set({ COOKIE_JAR: inJar });
+
+        console.log("new cookies in jar:");
+        const x = await this.getJarCookies();
+        console.dir(x);
+    }
+
+    async getJarCookies() {
+        const stored = await chrome.storage.local.get(COOKIE_JAR);
+        console.dir(stored);
+        return stored.COOKIE_JAR;
+    }
+}
+
+const cookieJar = new CookieJar();
+
+(async () => {
+    await cookieJar.init();
+})();
+
+class JarCookie {
     constructor(chromeCookie, isStored = false) {
-        this.domain = chromeCookie.domain;
+        if (chromeCookie.domain.charAt(0) == ".") {
+            this.domain = chromeCookie.domain.slice(1, -1);
+        } else {
+            this.domain = chromeCookie.domain;
+        }
         this.name = chromeCookie.name;
         this.storeId = chromeCookie.storeId;
         this.expirationDate = chromeCookie.expirationDate;
@@ -37,13 +101,16 @@ class Cookie {
 
         this.isStored = true;
 
+        console.log("storing cookie...");
         // Store the cookie in local storage
-        const key = JSON.stringify(this.details);
-        const cookieStorage = {};
-        cookieStorage[key] = this;
-        await chrome.storage.local.set(cookieStorage);
+        await cookieJar.addCookie(this);
+        // const key = JSON.stringify(this.details);
+        // const cookieStorage = {};
+        // cookieStorage[key] = this;
+        // await chrome.storage.local.set(cookieStorage);
 
         // Remove the cookie from chrome cookies
+        console.log("removing cookie from chrome");
         await chrome.cookies.remove(this.details);
     }
 
@@ -52,10 +119,7 @@ class Cookie {
         if (!this.isStored) return;
         this.isStored = false;
 
-        // Add the cookies back to chrome cookies
-        const key = JSON.stringify(this.details);
-        const storedCookie = chrome.storage.local.get(key);
-
+        console.log("adding cookie to chrome");
         await chrome.cookies.set({
             domain: this.domain,
             httpOnly: this.httpOnly,
@@ -68,19 +132,26 @@ class Cookie {
             value: this.value,
         });
 
-        // Remove the cookie from local storage
-        await chrome.storage.local.remove(key);
+        console.log("removing cookie from jar");
+        // Remove the cookie from the jar
+        await cookieJar.removeCookie(this.details);
+        // const key = JSON.stringify(this.details);
+        // const storedCookie = chrome.storage.local.get(key);
     }
 }
 
 async function getCookies() {
-    var chromeCookies = await chrome.cookies.getAll({});
-    const ourCookies = [];
+    const chromeCookies = await chrome.cookies.getAll({});
+    console.dir(chromeCookies);
+    let jarCookies = [];
 
     for (var cookie of chromeCookies) {
-        const ourCookie = new Cookie(cookie);
-        ourCookies.push(ourCookie);
+        const ourCookie = new JarCookie(cookie, false);
+        jarCookies.push(ourCookie);
     }
 
-    return ourCookies;
+    const j = await cookieJar.getJarCookies();
+    console.dir(j);
+    jarCookies = jarCookies.concat(j);
+    return jarCookies;
 }
--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,7 @@
 const cookieTable = document.getElementById("cookieTable");
 
 window.onload = async function () {
+    console.log("window loaded");
     await populateCookieTable();
 };
 
@@ -9,28 +10,19 @@ async function populateCookieTable() {
     for (const cookie of cookies) {
         const cookieTableRowItem = document.createElement("tr");
         cookieTableRowItem.style.marginBottom = "10px";
-        buttonStatus = "";
-        if (cookie.isStored) {
-            buttonStatus =
-                "<td><button disabled>Store</Button></td> <td><button >Restore</Button> </td>";
-        } else {
-            buttonStatus =
-                "<td><button >Store</Button></td> <td><button disabled>Restore</Button></td>";
-        }
-        cookieTableRowItem.innerHTML = `<td>${cookie.domain}</td> <td>${cookie.name}</td> <td>${cookie.path}</td>
-    <td>${cookie.secure}</td> <td>${cookie.session}</td> <td>${cookie.storeId}</td> <td>${cookie.url}`;
-
-        // var storeCell = cookieTableRowItem.insertCell(-1);
-        // storeCell.innerHTML = `<td><button ${cookie.isStored ? "disabled" : ""}>Store</Button></td>`;
-
-        // var restoreCell = cookieTableRowItem.insertCell(-1);
-        // restoreCell.innerHTML = `<td><button ${cookie.isStored ? "" : "disabled"}>Restore</Button></td>`;
+        cookieTableRowItem.innerHTML = `<td>${cookie.domain}</td> 
+                <td>${cookie.name}</td> 
+                <td>${cookie.path}</td>
+                <td>${cookie.secure}</td> 
+                <td>${cookie.session}</td> 
+                <td>${cookie.storeId}</td> 
+                <td>${cookie.url}`;
 
         storeCell = document.createElement("td");
         storeBtn = document.createElement("button");
         storeBtn.innerHTML = "Store";
         storeBtn.disabled = cookie.isStored;
-        storeBtn.addEventListener("click", cookie.store);
+        storeBtn.addEventListener("click", () => cookie.store());
         storeCell.appendChild(storeBtn);
         cookieTableRowItem.appendChild(storeCell);
 
@@ -38,7 +30,7 @@ async function populateCookieTable() {
         restoreBtn = document.createElement("button");
         restoreBtn.innerHTML = "Restore";
         restoreBtn.disabled = !cookie.isStored;
-        restoreBtn.addEventListener("click", cookie.restore);
+        restoreBtn.addEventListener("click", () => cookie.restore());
         restoreCell.appendChild(restoreBtn);
         cookieTableRowItem.appendChild(restoreCell);
 
