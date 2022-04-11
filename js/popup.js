4656b22401104d4fbdf8822015abe0e1878c64b5
Hamdan <s.hamdananwar@gmail.com>
2022-04-10 19:04:56 -0500

    Added onclick method for store and restore buttons

--- js/popup.js
+++ js/popup.js
@@ -18,25 +18,27 @@ async function populateCookieTable() {
     cookieTableRowItem.innerHTML = `<td>${cookie.domain}</td> <td>${cookie.name}</td> <td>${cookie.path}</td>
     <td>${cookie.secure}</td> <td>${cookie.session}</td> <td>${cookie.storeId}</td> <td>${cookie.url}`;
 
-    var storeCell = cookieTableRowItem.insertCell(-1);
-    storeCell.innerHTML = `<td><button ${cookie.isStored ? "disabled" : ""}>Store</Button></td>`;
+    // var storeCell = cookieTableRowItem.insertCell(-1);
+    // storeCell.innerHTML = `<td><button ${cookie.isStored ? "disabled" : ""}>Store</Button></td>`;
 
-    var restoreCell = cookieTableRowItem.insertCell(-1);
-    restoreCell.innerHTML = `<td><button ${cookie.isStored ? "" : "disabled"}>Restore</Button></td>`;
+    // var restoreCell = cookieTableRowItem.insertCell(-1);
+    // restoreCell.innerHTML = `<td><button ${cookie.isStored ? "" : "disabled"}>Restore</Button></td>`;
 
-    // storeCell = document.createElement("td");
-    // storeBtn = document.createElement("button");
-    // storeBtn.innerHTML = 'Store';
-    // storeBtn.disabled = cookie.isStored;
-    // storeCell.appendChild(storeBtn);
-    // cookieTableRowItem.appendChild(storeCell)
+    storeCell = document.createElement("td");
+    storeBtn = document.createElement("button");
+    storeBtn.innerHTML = 'Store';
+    storeBtn.disabled = cookie.isStored;
+    storeBtn.addEventListener("click", cookie.store);
+    storeCell.appendChild(storeBtn);
+    cookieTableRowItem.appendChild(storeCell)
 
-    // restoreCell = document.createElement("td");
-    // restoreBtn = document.createElement("button");
-    // restoreBtn.innerHTML = 'Restore';
-    // restoreBtn.disabled = !(cookie.isStored);
-    // restoreCell.appendChild(restoreBtn)
-    // cookieTableRowItem.appendChild(restoreCell)
+    restoreCell = document.createElement("td");
+    restoreBtn = document.createElement("button");
+    restoreBtn.innerHTML = 'Restore';
+    restoreBtn.disabled = !(cookie.isStored);
+    restoreBtn.addEventListener("click", cookie.restore);
+    restoreCell.appendChild(restoreBtn);
+    cookieTableRowItem.appendChild(restoreCell)
 
     cookieTable.appendChild(cookieTableRowItem);
     }
