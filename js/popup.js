775f8487b2c990ef8ba4cf0ec205974794673a20
Andrew Richerson <ajorigami1@gmail.com>
2022-04-14 02:51:36 -0500

    Prevent entire cookie table from being reloaded every time as per #3

--- js/popup.js
+++ js/popup.js
@@ -12,11 +12,7 @@ function removeLoadingIndicator() {
 }
 
 window.onload = async function () {
-    showLoadingIndicator();
-
     await populateCookieTable();
-
-    removeLoadingIndicator();
 };
 
 function truncateString(str, n) {
@@ -32,63 +28,66 @@ function alphabeticalComparison(a, b) {
     }
     return 0;
 }
-async function populateCookieTable() {
-    const cookies = await getCookies();
-    const sortedCookies = cookies.sort((a, b) =>
-        alphabeticalComparison(a.name, b.name)
-    );
-    console.log(`${sortedCookies.length} sorted cookies being displayed`);
-    for (const cookie of sortedCookies) {
-        const cookieTableRowItem = document.createElement("tr");
-        cookieTableRowItem.classList += "cookie-row";
-        cookieTableRowItem.style.marginBottom = "10px";
 
-        const truncatedName = truncateString(cookie.name, 40);
-        const truncatedVal = truncateString(cookie.value, 15);
-        const truncatedUrl = truncateString(cookie.details.url, 20);
-        cookieTableRowItem.innerHTML += `<td>${truncatedName}</td> 
+async function setCookieTableRowData(tableRow, cookie) {
+    const truncatedName = truncateString(cookie.name, 40);
+    const truncatedVal = truncateString(cookie.value, 15);
+    const truncatedUrl = truncateString(cookie.details.url, 20);
+    tableRow.innerHTML = `<td>${truncatedName}</td> 
                 <td>${truncatedVal}</td>
                 <td>${truncatedUrl}</td>
                 <td>${cookie.secure}</td> 
                 <td>${cookie.session}</td> 
                 <td>${cookie.storeId}</td>`;
 
-        storeCell = document.createElement("td");
-        storeBtn = document.createElement("button");
-        storeBtn.innerHTML = "Store";
-        storeBtn.disabled = cookie.isStored;
-        storeBtn.addEventListener("click", async () => {
-            showLoadingIndicator();
+    storeCell = document.createElement("td");
+    storeBtn = document.createElement("button");
+    storeBtn.innerHTML = "Store";
+    storeBtn.disabled = cookie.isStored;
+    storeBtn.addEventListener("click", async () => {
+        await cookie.store();
+        await setCookieTableRowData(tableRow, cookie);
+    });
+    storeCell.appendChild(storeBtn);
 
-            // For now, when the click the store button, we will store the cookie then reset the entire table. This is not performant
-            await cookie.store();
-            await resetCookieTable();
-            await populateCookieTable(); // Who said we wouldn't use recursion?
+    restoreCell = document.createElement("td");
+    restoreBtn = document.createElement("button");
+    restoreBtn.innerHTML = "Restore";
+    restoreBtn.disabled = !cookie.isStored;
+    restoreBtn.addEventListener("click", async () => {
+        await cookie.restore();
+        await setCookieTableRowData(tableRow, cookie);
+    });
+    restoreCell.appendChild(restoreBtn);
 
-            removeLoadingIndicator();
-        });
-        storeCell.appendChild(storeBtn);
+    // prepend adds element to beginning
+    tableRow.prepend(restoreCell);
+    tableRow.prepend(storeCell);
+}
 
-        restoreCell = document.createElement("td");
-        restoreBtn = document.createElement("button");
-        restoreBtn.innerHTML = "Restore";
-        restoreBtn.disabled = !cookie.isStored;
-        restoreBtn.addEventListener("click", async () => {
-            showLoadingIndicator();
+async function createCookieTableRow(cookie) {
+    const cookieTableRowItem = document.createElement("tr");
+    cookieTableRowItem.classList += "cookie-row";
+    cookieTableRowItem.style.marginBottom = "10px";
 
-            // See store handler for how this is not performant
-            await cookie.restore();
-            await resetCookieTable();
-            await populateCookieTable();
+    await setCookieTableRowData(cookieTableRowItem, cookie);
 
-            removeLoadingIndicator();
-        });
-        restoreCell.appendChild(restoreBtn);
+    return cookieTableRowItem;
+}
 
-        // prepend adds element to beginning
-        cookieTableRowItem.prepend(restoreCell);
-        cookieTableRowItem.prepend(storeCell);
+async function populateCookieTable() {
+    showLoadingIndicator();
 
+    const cookies = await getCookies();
+    const sortedCookies = cookies.sort((a, b) =>
+        alphabeticalComparison(a.name, b.name)
+    );
+    console.log(`${sortedCookies.length} sorted cookies being displayed`);
+
+    removeLoadingIndicator();
+
+    for (const cookie of sortedCookies) {
+        const cookieTableRowItem = await createCookieTableRow(cookie);
         cookieTable.appendChild(cookieTableRowItem);
     }
 }
