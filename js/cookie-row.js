6c802a6694ea59f8a5c39886aa18913c060b19d5
Andrew Richerson <ajorigami1@gmail.com>
2022-05-01 00:43:21 -0500

    Further refine mockup

--- js/cookie-row.js
+++ js/cookie-row.js
@@ -1,15 +1,15 @@
-const cookieRowContent = document.getElementById("TEST-cookie-row-content");
+const cookieRowContent = document.getElementById("TEST-cookie-row-header");
 const cookieInfo = document.getElementById("TEST-cookie-info");
+const testCaret = document.getElementById("TEST-caret");
+
+cookieInfo.style.display = "none";
 
 cookieRowContent.addEventListener("click", () => {
-    console.log("cookie row clicked");
     if (cookieInfo.style.display == "none") {
-        cookieInfo.style.display = "flex";
+        cookieInfo.style.display = "block";
+        testCaret.src = "/assets/caret-down.png";
     } else {
         cookieInfo.style.display = "none";
+        testCaret.src = "/assets/caret-right.png";
     }
 });
-
-// function cookieRow(/* cookie */){
-
-// }
