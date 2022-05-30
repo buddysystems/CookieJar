bbb6dec482b3994915ff2d10a2295aa5dbf69b1f
ckildare <98177297+ckildare@users.noreply.github.com>
2022-05-30 11:36:50 -0500

    Expaded tests on cookies manager methods

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
-import {CookiesManager} from "../js/cookies/cookies-manager.js";
+import { CookiesManager } from "../js/cookies/cookies-manager.js";
+import { expect } from 'chai';
 
 const cookies = [
   {
@@ -83,11 +84,59 @@ const cookies = [
   },
 ]
 
-const selector = CookiesManager
+const manager = new CookiesManager
 
 describe("CookiesManager", () => {
 
-  it("Test ", () => {
+  it("Test storing many cookies", () => {
+    const firstCookie = {
+      name: "SNID",
+      domain: ".google.com",
+      storeId: "0",
+      expirationDate: 1669169444.091988,
+      hostOnly: false,
+      httpOnly: true,
+      path: "/verify",
+      sameSite: "lax",
+      secure: true,
+      session: false,
+      value: "APx-0P1IoMLMgru1xnM4haDuzkPnqgV7CoUIK8pwMQTKHr-QESGocu98_D-nRn7kFsgL-qNrtDy3Ozs__AHMyfEbXKBY",
+      details: {
+        name: "SNID",
+        storeId: "0",
+        url: "https://.google.com/verify"
+      },
+      isStored: false,
+      isSelected: false
+    }
+    const secondCookie = {
+      name: "Lets Gooo",
+      domain: ".google.com",
+      storeId: "0",
+      expirationDate: 1669169444.091988,
+      hostOnly: false,
+      httpOnly: true,
+      path: "/verify",
+      sameSite: "lax",
+      secure: true,
+      session: false,
+      value: "APx-0P1IoMLMgru1xnM4haDuzkPnqgV7CoUIK8pwMQTKHr-QESGocu98_D-nRn7kFsgL-qNrtDy3Ozs__AHMyfEbXKBY",
+      details: {
+        name: "SNID",
+        storeId: "0",
+        url: "https://.google.com/verify"
+      },
+      isStored: false,
+      isSelected: true
+    }
+    const testCookies = [firstCookie, secondCookie];
+
+    manager.storeCookies(testCookies)
+    expect(firstCookie.isStored).to.be.true;
+    expect(secondCookie.isStored).to.be.true;
+  });
+
+  it("Test storing a single cookie", () => {
     const testCookie = {
       name: "SNID",
       domain: ".google.com",
@@ -109,11 +158,177 @@ describe("CookiesManager", () => {
       isSelected: false
     };
 
-    selectCookie(testCookie)
-    expect(testCookie.isSelected).toBeTruthy();
+    manager.storeCookie(testCookie)
+    expect(testCookie.isStored).to.be.true;
   });
 
-  it("it should filter by a search term (link)", () => {
-    const cookies = {
-  }
-})});
+  it("Test restoring many cookies", () => {
+    const firstCookie = {
+      name: "SNID",
+      domain: ".google.com",
+      storeId: "0",
+      expirationDate: 1669169444.091988,
+      hostOnly: false,
+      httpOnly: true,
+      path: "/verify",
+      sameSite: "lax",
+      secure: true,
+      session: false,
+      value: "APx-0P1IoMLMgru1xnM4haDuzkPnqgV7CoUIK8pwMQTKHr-QESGocu98_D-nRn7kFsgL-qNrtDy3Ozs__AHMyfEbXKBY",
+      details: {
+        name: "SNID",
+        storeId: "0",
+        url: "https://.google.com/verify"
+      },
+      isStored: true,
+      isSelected: false
+    }
+    const secondCookie = {
+      name: "Lets Gooo",
+      domain: ".google.com",
+      storeId: "0",
+      expirationDate: 1669169444.091988,
+      hostOnly: false,
+      httpOnly: true,
+      path: "/verify",
+      sameSite: "lax",
+      secure: true,
+      session: false,
+      value: "APx-0P1IoMLMgru1xnM4haDuzkPnqgV7CoUIK8pwMQTKHr-QESGocu98_D-nRn7kFsgL-qNrtDy3Ozs__AHMyfEbXKBY",
+      details: {
+        name: "SNID",
+        storeId: "0",
+        url: "https://.google.com/verify"
+      },
+      isStored: true,
+      isSelected: true
+    }
+    const testCookies = [firstCookie, secondCookie];
+
+    manager.restoreCookies(testCookies)
+    expect(firstCookie.isStored).to.be.false;
+    expect(secondCookie.isStored).to.be.false;
+  });
+
+  it("Test restoring a single cookie", () => {
+    const testCookie = {
+      name: "SNID",
+      domain: ".google.com",
+      storeId: "0",
+      expirationDate: 1669169444.091988,
+      hostOnly: false,
+      httpOnly: true,
+      path: "/verify",
+      sameSite: "lax",
+      secure: true,
+      session: false,
+      value: "APx-0P1IoMLMgru1xnM4haDuzkPnqgV7CoUIK8pwMQTKHr-QESGocu98_D-nRn7kFsgL-qNrtDy3Ozs__AHMyfEbXKBY",
+      details: {
+        name: "SNID",
+        storeId: "0",
+        url: "https://.google.com/verify"
+      },
+      isStored: true,
+      isSelected: false
+    };
+
+    manager.restoreCookie(testCookie)
+    expect(testCookie.isStored).to.be.false;
+  });
+
+  it("Test that getJarredCookies works properly", () => {
+    const jarCookies = {
+      name: "SNID",
+      domain: ".google.com",
+      storeId: "0",
+      expirationDate: 1669169444.091988,
+      hostOnly: false,
+      httpOnly: true,
+      path: "/verify",
+      sameSite: "lax",
+      secure: true,
+      session: false,
+      value: "APx-0P1IoMLMgru1xnM4haDuzkPnqgV7CoUIK8pwMQTKHr-QESGocu98_D-nRn7kFsgL-qNrtDy3Ozs__AHMyfEbXKBY",
+      details: {
+        name: "SNID",
+        storeId: "0",
+        url: "https://.google.com/verify"
+      },
+      isStored: true,
+      isSelected: false
+    };
+
+    const chromeCookies = {
+      name: "Lets Gooo",
+      domain: ".google.com",
+      storeId: "0",
+      expirationDate: 1669169444.091988,
+      hostOnly: false,
+      httpOnly: true,
+      path: "/verify",
+      sameSite: "lax",
+      secure: true,
+      session: false,
+      value: "APx-0P1IoMLMgru1xnM4haDuzkPnqgV7CoUIK8pwMQTKHr-QESGocu98_D-nRn7kFsgL-qNrtDy3Ozs__AHMyfEbXKBY",
+      details: {
+        name: "SNID",
+        storeId: "0",
+        url: "https://.google.com/verify"
+      },
+      isStored: false,
+      isSelected: true
+    };
+
+    const cookiesManager = new CookiesManager(jarCookies, chromeCookies);
+    const returnedCookie = cookiesManager.getJarredCookies()
+    expect(returnedCookie).to.equal(jarCookies.details);
+  });
+
+  it("Test that cookies update properly", () => {
+    const jarCookies = {
+      name: "SNID",
+      domain: ".google.com",
+      storeId: "0",
+      expirationDate: 1669169444.091988,
+      hostOnly: false,
+      httpOnly: true,
+      path: "/verify",
+      sameSite: "lax",
+      secure: true,
+      session: false,
+      value: "APx-0P1IoMLMgru1xnM4haDuzkPnqgV7CoUIK8pwMQTKHr-QESGocu98_D-nRn7kFsgL-qNrtDy3Ozs__AHMyfEbXKBY",
+      details: {
+        name: "SNID",
+        storeId: "0",
+        url: "https://.google.com/verify"
+      },
+      isStored: true,
+      isSelected: false
+    };
+
+    const chromeCookies = {
+      name: "Lets Gooo",
+      domain: ".google.com",
+      storeId: "0",
+      expirationDate: 1669169444.091988,
+      hostOnly: false,
+      httpOnly: true,
+      path: "/verify",
+      sameSite: "lax",
+      secure: true,
+      session: false,
+      value: "APx-0P1IoMLMgru1xnM4haDuzkPnqgV7CoUIK8pwMQTKHr-QESGocu98_D-nRn7kFsgL-qNrtDy3Ozs__AHMyfEbXKBY",
+      details: {
+        name: "SNID",
+        storeId: "0",
+        url: "https://.google.com/verify"
+      },
+      isStored: false,
+      isSelected: true
+    };
+
+    const cookiesManager = new CookiesManager(jarCookies, chromeCookies);
+    const returnedCookie = cookiesManager.getJarredCookies()
+    expect(returnedCookie).to.equal(jarCookies.details);
+  });
+});
